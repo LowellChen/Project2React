2a8468cde7aa24fae266e95d98eabf2ba21cf52f
LowellChen <yjtanos@yahoo.com.sg>
2022-11-24 09:55:00 +0000

    added form

---
+++ app.js
@@ -0,0 +1,24 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import wine from "./frontend.js";
+export default function App() {
+ return <React.Fragment>
+   <wine/>
+ </React.Fragment>;
+}
+
+
+
+
